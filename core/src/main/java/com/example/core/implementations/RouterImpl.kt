8d70a634a75859d2f3906f28efe5ade3c9dc2ada
SergJ4 <sergjav@gmail.com>
2018-12-30 15:05:05 +0700

    Started app backbone

---
+++ core/src/main/java/com/example/core/implementations/RouterImpl.kt
@@ -0,0 +1,49 @@
+package com.example.core.implementations
+
+import androidx.fragment.app.Fragment
+import com.example.core.interfaces.navigation.NavigationModule
+import com.example.core.interfaces.navigation.Router
+import ru.terrakok.cicerone.Screen
+import ru.terrakok.cicerone.android.support.SupportAppScreen
+
+internal class RouterImpl(private val router: ru.terrakok.cicerone.Router) : Router {
+
+    private val modules = mutableSetOf<NavigationModule>()
+
+    override fun installModule(module: NavigationModule) {
+        modules.add(module)
+    }
+
+    override fun uninstallModule(module: NavigationModule) {
+        modules.remove(module)
+    }
+
+    override fun goTo(screen: com.example.core.interfaces.navigation.Screen) {
+        val module = modules.firstOrNull { it.isScreenNameSupported(screen.screenName) }
+        if (module == null) {
+            throw IllegalStateException("No module provided to navigate to $screen.screenName")
+        } else {
+            router.navigateTo(createScreen(module, screen))
+        }
+    }
+
+    override fun rootScreen(screen: com.example.core.interfaces.navigation.Screen) {
+        val module = modules.firstOrNull { it.isScreenNameSupported(screen.screenName) }
+        if (module == null) {
+            throw IllegalStateException("No module provided to navigate to $screen.screenName")
+        } else {
+            router.newRootScreen(createScreen(module, screen))
+        }
+    }
+
+    override fun back() = router.exit()
+}
+
+private fun createScreen(
+    module: NavigationModule,
+    screen: com.example.core.interfaces.navigation.Screen
+): Screen = object : SupportAppScreen() {
+    override fun getFragment(): Fragment {
+        return module.getFragment(screenName = screen.screenName, data = screen.data)
+    }
+}
---
+++ core/src/main/java/com/example/core/interfaces/navigation/Screen.kt
@@ -0,0 +1,8 @@
+package com.example.core.interfaces.navigation
+
+interface Screen {
+
+    val screenName: String
+
+    val data: Any?
+}
