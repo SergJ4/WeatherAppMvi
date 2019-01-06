516eb9546757b47dbb93866189349410d38195cf
SergJ4 <sergjav@gmail.com>
2019-01-06 19:39:28 +0700

    ViewModel and Bindings for details fragment

--- weathersingle/src/main/java/com/example/weathersingle/DetailsFragmentViewModel.kt
+++ weathersingle/src/main/java/com/example/weathersingle/DetailsFragmentViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.weathersingle
 
 import androidx.lifecycle.ViewModel
-import com.example.core.interfaces.Router
+import com.badoo.mvicore.extension.mapNotNull
+import com.example.core.SchedulersProvider
+import com.example.core.models.City
+import eu.davidea.flexibleadapter.items.IFlexible
+import io.reactivex.Observable
+import io.reactivex.ObservableSource
+import io.reactivex.functions.Consumer
 
-class DetailsFragmentViewModel(router: Router) : ViewModel() {
+class DetailsFragmentViewModel(detailsFragmentFeature: DetailsFragmentFeature) :
+    ViewModel() {
+
+    private val outputMapper: (DetailsFragmentFeature.State) -> DetailsFragment.Model = {
+        DetailsFragment.Model(listOf(), it.isLoading)
+    }
+
+    val input: Consumer<DetailsFragment.UiEvent> = Consumer { event ->
+        when (event) {
+            DetailsFragment.UiEvent.Refresh -> detailsFragmentFeature.accept(DetailsFragmentFeature.Wish.Refresh)
+            DetailsFragment.UiEvent.BackClicked -> detailsFragmentFeature.accept(
+                DetailsFragmentFeature.Wish.Back
+            )
+        }
+    }
+
+    val output: ObservableSource<DetailsFragment.Model> = Observable
+        .wrap(detailsFragmentFeature)
+        .mapNotNull(outputMapper)
+        .subscribeOn(SchedulersProvider.computation())
+        .observeOn(SchedulersProvider.ui())
+
+    private fun createItems(cities: List<City>): List<IFlexible<*>> =
+        TODO()
 }
